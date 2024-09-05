e42ff80ab83c5a8531726e1600c07ecfa00f2bba
matheusgouvea <matheusgouvea@live.com>
2024-09-05 15:54:33 -0300

    Exercicio 4 - Lista 3

--- exercicios_1/exerciciosarthur/src/main/java/com/poo/lista3/Exercicio4.java
+++ exercicios_1/exerciciosarthur/src/main/java/com/poo/lista3/Exercicio4.java
@@ -1,6 +1,8 @@
 package com.poo.lista3;
 
-/* */
+/*4) Jogo da adivinhação. Peça para alguém escolher um número. Depois peça
+para outra pessoa tentar adivinhar. O programa deverá retornar dicas como:
+"Muito baixo, muito alto ou quase lá" ou "tá quente, tá frio". */
 
 import java.util.Scanner;
 
